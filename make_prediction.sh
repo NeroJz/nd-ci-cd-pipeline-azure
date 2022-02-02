3b9036dce98e4902761801ac5202607f9fb18166
KumHoe Lau <nerojz86@gmail.com>
2022-02-02 21:50:10 +0800

    Update endpoint

--- make_prediction.sh
+++ make_prediction.sh
@@ -25,4 +25,4 @@ curl -d '{
    }
 }'\
      -H "Content-Type: application/json" \
-     -X POST http://localhost:$PORT/predict
+     -X POST https://jz-udacity-azure-devop-project-2.azurewebsites.net:$PORT/predict
