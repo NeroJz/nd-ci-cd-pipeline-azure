01633ccb212d6b180ce103dc1e9ec9cda6875ee3
KumHoe Lau <nerojz86@gmail.com>
2022-02-02 21:52:35 +0800

    Fixed endpoint on make_predict_azure_app.sh

--- make_predict_azure_app.sh
+++ make_predict_azure_app.sh
@@ -25,5 +25,5 @@ curl -d '{
    }
 }'\
      -H "Content-Type: application/json" \
-     -X POST https://jz-udacity-azure-devop-project-2.azurewebsites.net/:$PORT/predict 
+     -X POST https://jz-udacity-azure-devop-project-2.azurewebsites.net:$PORT/predict 
      #your application name <yourappname>goes here
